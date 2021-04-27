2a84542ae7f4c393525aa2d2cc6ffad6b7dfcd68
Mark La Torre <mlatorre@pucp.pe>
2021-04-26 23:39:20 -0500

    only missing the video

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+function showLanding(){
+    var img = document.getElementById("picture");
+    img.src = "./images/landing-screenshot.png";
+}
+
+function showDashboard(){
+    var img = document.getElementById("picture");
+    img.src = "./images/dashboard-screenshot.png";
+}
+
+function showChatroom(){
+    var img = document.getElementById("picture");
+    img.src = "./images/chatroom-screenshot.png";
+}
+
+
+//<img id="landing-screenshot" src="./images/landing-screenshot.png" width="50%" height="50%" style="visibility: hidden"> 
+//<img id="dashboard-screenshot" src="./images/dashboard-screenshot.png" width="50%" height="50%" style="visibility: hidden"> 
+// <img id="chatroom-screenshot" src="./images/chatroom-screenshot.png" width="50%" height="50%" style="visibility: hidden"> 
